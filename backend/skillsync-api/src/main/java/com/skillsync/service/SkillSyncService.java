6992f5c829cb6aa70355d4b2c627a77bac4a84d9
Berserk@96k <dhruvombale@gmail.com>
2025-07-09 18:45:11 +0530

    🔥 Updated UserService and refactored logic

--- backend/skillsync-api/src/main/java/com/skillsync/service/SkillSyncService.java
+++ backend/skillsync-api/src/main/java/com/skillsync/service/SkillSyncService.java
@@ -1,5 +1,8 @@
 package com.skillsync.service;
 
+import com.skillsync.model.User;
+
 public interface SkillSyncService {
 
+	User getAll();
 }
--- backend/skillsync-api/src/main/java/com/skillsync/service/SkillSyncServiceImplementation.java
+++ backend/skillsync-api/src/main/java/com/skillsync/service/SkillSyncServiceImplementation.java
@@ -1,5 +1,14 @@
 package com.skillsync.service;
 
+import com.skillsync.model.User;
+
 public class SkillSyncServiceImplementation implements SkillSyncService {
 
+	@Override
+	public User getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
